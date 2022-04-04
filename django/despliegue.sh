90d6522028071d88d8e7d896e846d47da559bc42
Davidzf21 <zandundo2a@gmail.com>
2022-04-04 19:29:37 +0200

    Create despliegue.sh para el despliegue automatico de la aplicacion

---
+++ django/despliegue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+######################################################################
+# @author      : grupo 5
+# @file        : despliegue.sh
+# @created     : domingo feb 28, 2021 19:19:01 CET
+#
+# @description : Despliegue de repositorio git en heroku
+######################################################################
+#Actualizar librerías necesarias para heroku
+pip freeze > requirements.txt
+#Actualizar repositorio git
+git add .
+echo "Añade un comentario al git commit: "
+read comentario
+git commit -m "$comentario"
+#Actualizar commit en heroku
+heroku maintenance:on
+git push -f heroku
+heroku maintenance:off
+#Visualizar aplicación desplegada
+heroku open
+
+#ver log del servidor en producción
+#heroku logs --tail --app rocky-waters-55301
+#Probar despliegue en local
+#heroku local
+#Dejar de dar servicio
+#heroku ps:scale web=0
