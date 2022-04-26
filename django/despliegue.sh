2f6931c250086b952007d65328d1b218685116a1
Alv316 <76782359+Alv316@users.noreply.github.com>
2022-04-26 11:47:47 +0200

    Proyecto cambiado de univibesPr a itreaderPr

--- django/despliegue.sh
+++
@@ -1,8 +0,0 @@
-heroku login #solo una vez
-git init #solo una vez, para crear un repositorio
-heroku git:remote -a db-itreader-unizar
-git add .
-git commit -m "mensaje"
-git push heroku master
-
-git push -f heroku
--- django/utils/QuickStart.py
+++
@@ -1,4 +0,0 @@
-from pydrive2.auth import GoogleAuth
-
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
